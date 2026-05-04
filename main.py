a2faac64a5bfb9995bd7e480a46233c7a3f3f152
oliver Lumono <oliverlumon8@gmail.com>
2026-05-04 16:19:30 +0300

    add jinja tmplate

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def welcome():
-    # Make sure 'index.html' exists in a folder named 'templates'
     return render_template('index.html')
 
 @app.route('/success/<int:score>')
 def success(score):
-    return f"The person has passed and the mark is {score}"
+    res = ''
+    if score >= 50:
+        res = 'Passed'
+    else:
+        res = 'Failed'
+    exp={'score':score, 'res':res}
+    return render_template('result.html', result=exp)
 
 @app.route('/failure/<int:score>')
 def failure(score):
@@ -17,16 +22,26 @@ def failure(score):
 
 @app.route('/results/<int:score>')
 def results(score):
-    # Removed the unused 'results' variable
+    
     if score < 50:
         endpoint = 'failure'
+    
     else:
         endpoint = 'success'
     return redirect(url_for(endpoint, score=score))
-###result checker html page
-#@app.route('/submit', methods=['POST', 'GET'])
-#def submit():
 
-
+@app.route('/submit', methods=['POST', 'GET'])
+def submit():
+    total_score=0
+    if request.method =='POST':
+        science=float(request.form['science'])
+        math=float(request.form['math'])
+        C=float(request.form['C'])
+        data_science=float(request.form['data_science'])
+        total_score=(science+math+C+data_science)/4
+    res=''
+    
+    return redirect(url_for('success', score=total_score))
+         
 if __name__ == '__main__':
     app.run(debug=True)
